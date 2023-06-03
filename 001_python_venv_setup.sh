5e6658d45c82a854b7418023ae31b5195f7906f7
cham <cham3.ky.my>
2023-06-03 19:34:33 +0800

    update python pytest unit test

---
+++ 001_python_venv_setup.sh
@@ -0,0 +1,37 @@
+# Installer Python 3.11.3
+# Refer: https://www.python.org/downloads/release/python-3113/
+
+# Check python version
+python --version
+
+# Installl virtualenv
+pip install virtualenv
+
+# Change directory to project folder
+cd C:\Users\cham\OneDrive\Documents\Project\SQA
+
+# Create virtual environment
+virtualenv  --python C:\Users\cham\AppData\Local\Programs\Python\Python311\python.exe venv
+
+# Run as administrator
+# To allow execution of PowerShell scripts:
+Set-ExecutionPolicy Unrestricted -Force
+
+## Reload VS code
+
+# Change directory to project folder
+cd C:\Users\cham\OneDrive\Documents\Project\SQA
+
+## Activate virtual environment
+venv\Scripts\activate.ps1
+
+# Change directory to unit test folder
+cd C:\Users\cham\OneDrive\Documents\Project\SQA\unit_test
+
+# Install pytest
+pip install pytest
+
+## Remark
+#Running pytest without mentioning a filename will run all files of format test_*.py or *_test.py in the current directory and subdirectories. Pytest automatically identifies those files as test files. We can make pytest run other filenames by explicitly mentioning them.
+
+#Pytest requires the test function names to start with test. Function names which are not of format test* are not considered as test functions by pytest. We cannot explicitly make pytest consider any function not starting with test as a test function.
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SQA
+1. [x] Unit Testing
+    * Date: 03 Jun 20236
+    - We decide using python "pytest" to do our unit test
+    - pytest: When we run the command "pytest," all folders in the current directory whose names contain the word "test" (e.g., test or test) will be executed accordingly. But python file like this "testmul" will not be executed
+    - Then, all the functiion that contain the word "test" and format like test* or test_* will be run and show the result
+2. [ ] Api Testing
+3. [ ] Website Testing
